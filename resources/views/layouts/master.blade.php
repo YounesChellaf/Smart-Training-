68919fec79f18de2541cc1e7146e0286502725ba
younes chellaf <32979396+YounesChellaf@users.noreply.github.com>
2019-03-09 21:42:17 +0100

    Update master.blade.php
    HTTPS fonts fixed

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,14 +9,14 @@
     <link href="{{ asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
     <!-- Custom fonts for this template -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="http://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
     <link href="{{ asset('assets/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
 
 
     <!-- Custom styles for this template -->
